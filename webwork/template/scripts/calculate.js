2bc05635bd1e8315840ea95cc1448e0246dc9293
florenceman <71645157+florenceman@users.noreply.github.com>
2022-01-12 16:56:28 -0500

    set up modification for template to work with project
    added letter frequency

--- webwork/template/scripts/calculate.js
+++ webwork/template/scripts/calculate.js
@@ -1,3 +1,34 @@
 var calculate_result = function(){
-	return 'Placeholder result.';
+	var total_amount_of_sample = 1000041,
+	sample_letters = {
+	a:84966,
+	b:20720,
+	c:45388,
+	d:33844,
+	e:18121,
+	f:111607,
+	g:24705,
+	h:30034,
+	i:75488,
+	j:1965,
+	k:11016,
+	l:54893,
+	m:30129,
+	n:66544,
+	o:71635,
+	p:31671,
+	q:1962,
+	r:75809,
+	s:57351,
+	t:69509,
+	u:36308,
+	v:10074,
+	w:12899,
+	x:2902,
+	y:17779,
+	z:2722
+},
+	list_size = $('#in_1').val();
+	word_to_input = $('#in_2').val();
+	return list_size+','+word_to_input;
 };
