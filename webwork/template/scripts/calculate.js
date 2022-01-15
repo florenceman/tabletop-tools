d410c8d76c457b636e37c5995e8028d5d00c1da7
florenceman <71645157+florenceman@users.noreply.github.com>
2022-01-15 11:09:49 -0500

    added calculations

--- webwork/template/scripts/calculate.js
+++ webwork/template/scripts/calculate.js
@@ -1,34 +1,59 @@
 var calculate_result = function(){
 	var total_amount_of_sample = 1000041,
-	sample_letters = {
-	a:84966,
-	b:20720,
-	c:45388,
-	d:33844,
-	e:18121,
-	f:111607,
-	g:24705,
-	h:30034,
-	i:75488,
-	j:1965,
-	k:11016,
-	l:54893,
-	m:30129,
-	n:66544,
-	o:71635,
-	p:31671,
-	q:1962,
-	r:75809,
-	s:57351,
-	t:69509,
-	u:36308,
-	v:10074,
-	w:12899,
-	x:2902,
-	y:17779,
-	z:2722
-},
-	list_size = $('#in_1').val();
-	word_to_input = $('#in_2').val();
-	return list_size+','+word_to_input;
+	alphabet = 'abcdefghijklmnopqrstuvwxyz',
+	sample_letters = [84966,
+	20720,
+	45388,
+	33844,
+	18121,
+	111607,
+	24705,
+	30034,
+	75488,
+	1965,
+	11016,
+	54893,
+	30129,
+	66544,
+	71635,
+	31671,
+	1962,
+	75809,
+	57351,
+	69509,
+	36308,
+	10074,
+	12899,
+	2902,
+	17779,
+	2722
+],
+	total_so_far = [],
+	list_size = $('#in_1').val(),
+	word_to_input = $('#in_2').val(),
+	index_count = 1,
+	x = 0,
+	y = 0;
+
+	var number_scale = parseInt(total_amount_of_sample / list_size);
+	if (total_amount_of_sample % list_size){number_scale++;}
+	var scaled_letters = [];
+	
+
+	for (var v in scaled_letters){
+		x = parseInt(sample_letters[v] / total_amount_of_sample);
+		y = sample_letters[v] % total_amount_of_sample;
+		if (y){x++;}
+		if (!(x)){x=1;}
+		scaled_letters.push(x);
+	}
+	
+	for (var v in scaled_letters){
+		total_so_far.push(index_count);
+		index_count += scaled_letters[v];
+	}
+	
+	x = total_so_far[alphabet.indexOf(word_to_input.charAt(0))];
+	
+	return x;
 };
